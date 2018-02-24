2d7d54f189698169117b93781afa68dd45e27c2c
ishanka25 <ishan.ranatunga@gmail.com>
2018-02-24 21:44:02 +0530

    created herolist

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import CharacterList from './CharacterList';
+import HeroList from "./HeroList";
+import '../styles/index.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Justice League</h2>
+                <div className="col-md-6">
+                    <CharacterList />
+                </div>
+                <div className="col-md-6">
+                    <HeroList />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
